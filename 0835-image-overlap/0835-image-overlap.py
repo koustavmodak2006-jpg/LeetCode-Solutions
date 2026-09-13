eb31121d2129771e0019b1bf33e7eeb3fb48c3f1
Koustav Modak <koustavmodak2006@gmail.com>
2026-09-13 10:47:02 +0530

    Time: 220 ms (67.19%), Space: 19.7 MB (64.29%) - LeetHub

---
+++ 0835-image-overlap/0835-image-overlap.py
@@ -0,0 +1,25 @@
+from collections import Counter
+
+class Solution:
+    def largestOverlap(self, img1: list[list[int]], img2: list[list[int]]) -> int:
+        ones1 = []
+        ones2 = []
+
+        n = len(img1)
+
+        # Store coordinates of all 1s
+        for r in range(n):
+            for c in range(n):
+                if img1[r][c] == 1:
+                    ones1.append((r, c))
+                if img2[r][c] == 1:
+                    ones2.append((r, c))
+
+        # Count how many pairs have the same relative shift
+        shifts = Counter()
+
+        for r1, c1 in ones1:
+            for r2, c2 in ones2:
+                shifts[(r1 - r2, c1 - c2)] += 1
+
+        return max(shifts.values(), default=0)
